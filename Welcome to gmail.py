5843175a2059ec0149ca7c33e9f3221d8e6f5485
jackketteler <34751536+jackketteler@users.noreply.github.com>
2018-01-09 14:41:08 -0500

    Add files via upload

---
+++ Welcome to gmail.py
@@ -0,0 +1,16 @@
+print("Welcome to Gmail")
+myUsername =input("Enter your username, then press Return: ")
+
+if myUsername=="jackketteler":
+      print("Username successful")
+      myPassword=input("Enter your password, then press Return: ")
+      if myPassword=="sof123@":
+          print("Success")
+      else:
+          print("Unable to log in. Incorrect password")
+else:
+      print("Incorrect. Try again")
+      
+      
+
+      
