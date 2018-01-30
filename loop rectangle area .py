1c2ba63b6fd2eb6a72d4a06620798124aebf1c7c
jackketteler <34751536+jackketteler@users.noreply.github.com>
2018-01-30 14:00:35 -0500

    Add files via upload

---
+++ loop rectangle area .py
@@ -0,0 +1,40 @@
+
+"""
+build a calculator that provides the
+amount of miles per gallon
+
+miles per gallon = miles driven/gallons used
+"""
+"""
+miles_driven = 240
+gallons_used = 4
+
+while True:
+    print("This program calulates mpg.")
+    miles_driven = float(input("Enter miles driven: "))
+    gallons_used = float(input("Enter gallons used: "))
+    mpg= miles_driven / gallons_used
+    print("Your miles per gallon is", mpg)
+    user_input= input("Would you like to perform a new calculation? Y/N")
+    if user_input== "Y" or user_input== "y":
+        continue
+    else:
+        break
+"""
+
+# create a program that calculates the area of a rectangle
+
+while True:
+    print("This program calculates the area of a rectangle.")
+    length_rect = float(input("Enter the length of the rectangle: "))
+    width_rect = float(input("Enter the width of the rectangle: "))
+    area= length_rect * width_rect
+    print("Your area is", area)
+    user_input= input("Would you like to perform another calculation? Y/N")
+    if user_input== "Y" or user_input "y":
+        continue
+    else:
+        break
+    
+
+
